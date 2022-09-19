3747e5b3b4b76edf591d33fbcba3e236017c21f9
SKORPION29 <sayansarkar29.slg@gmail.com>
2022-09-19 19:10:28 +0000

    miatoll: ProjectBlaze Bringup
    Signed-off-by: SKORPION29 <sayansarkar29.slg@gmail.com>

--- blaze_miatoll.mk
+++ blaze_miatoll.mk
@@ -7,8 +7,8 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common Elixir stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common ProjectBlaze stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -17,7 +17,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
 # Device identifier
-PRODUCT_NAME := aosp_miatoll
+PRODUCT_NAME := blaze_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll
@@ -25,7 +25,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 CUSTOM_DEVICE := miatoll
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-WITH_GMS := true
 
 IS_PHONE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
@@ -34,6 +33,3 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 
-# Elixir
-ELIXIR_MAINTAINER := Lopster_70
-ELIXIR_BUILD_TYPE := OFFICIAL
